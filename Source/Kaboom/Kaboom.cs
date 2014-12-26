c24fde359fbcc301b36e261d555d0853154d87f3
Russ Nash <russnash37@yahoo.com>
2014-12-26 14:27:57 -0500

    Added delayed Kaboom with tweakables.

--- Source/Kaboom/Kaboom.cs
+++ Source/Kaboom/Kaboom.cs
@@ -8,10 +8,53 @@ namespace Kaboom
 {
     public class Kaboom : PartModule
     {
-        [KSPEvent(guiActive = true, guiName = "Kaboom!")]
-        public void KaboomPart()
+        [KSPField(isPersistant = true, guiActiveEditor = true, guiActive = true, guiName = "Kaboom delay", guiUnits = "Seconds"), UI_FloatRange(minValue = 0f, maxValue = 30f, stepIncrement = 1f)]
+        public float delay = 0;
+
+        [KSPField(isPersistant = true)]
+        public bool timerActive = false;
+
+        [KSPField(isPersistant = true)]
+        public double kaboomTime;
+
+        [KSPEvent(guiActive = true, guiActiveUnfocused = true, unfocusedRange = 2f, guiName = "Kaboom!")]
+        public void KaboomEvent()
         {
-            part.explode();
+            KaboomIt();
+        }
+
+        [KSPAction("Kaboom!")]
+        public void KaboomAction(KSPActionParam param)
+        {
+            KaboomIt();
+        }
+
+        private void KaboomIt()
+        {
+            part.force_activate();
+
+            if (delay == 0)
+            {
+                part.explode();
+            }
+            else
+            {
+                kaboomTime = Planetarium.GetUniversalTime() + delay;
+                timerActive = true;
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnFixedUpdate();
+            if (timerActive)
+            {
+                if (Planetarium.GetUniversalTime() >= kaboomTime)
+                {
+                    timerActive = false;
+                    part.explode();
+                }
+            }
         }
     }
 }
